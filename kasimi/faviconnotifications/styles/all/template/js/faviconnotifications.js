28aeeb99b49d2bca65710b58a501683b95bc7384
kasimi <mail@kasimi.net>
2016-08-23 16:11:00 +0200

    Hook into phpbb.markNotifications

---
+++ kasimi/faviconnotifications/styles/all/template/js/faviconnotifications.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * @package phpBB Extension - Favicon Notifications
+ * @copyright (c) 2016 kasimi
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+jQuery(function($) {
+	// Initialize favicon
+	var favicon = new Favico({
+		bgColor		: '#d00',
+		textColor	: '#fff',
+		fontFamily	: 'sans-serif',
+		fontStyle	: 'bold',
+		type		: 'circle',
+		position	: 'up',
+		animation	: 'popFade'
+	});
+
+	var setUnreadNotificationsCount = function(count) {
+		favicon.badge(count);
+	};
+
+	// Set initial notifications count
+	setUnreadNotificationsCount(kasimiFaviconNotifications.count);
+
+	// Update notifications count when marking notifications read
+	var phpbbMarkNotifications = phpbb.markNotifications;
+	phpbb.markNotifications = function($popup, unreadCount) {
+		setUnreadNotificationsCount(unreadCount);
+		return phpbbMarkNotifications($popup, unreadCount);
+	};
+
+	// Listen to notifications count updates from extensions
+	$(phpbb).on('kasimi_faviconnotifications_update', function(event, notificationsCount) {
+		setUnreadNotificationsCount(notificationsCount);
+	});
+});
